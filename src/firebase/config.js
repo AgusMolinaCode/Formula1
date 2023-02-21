5b4b7b7b97c613b7d623fd50bd9e9c1a6543685e
Agustin Molina <agustinmolina@MacBook-Air-de-Agustin.local>
2023-02-21 14:19:27 -0300

    1.1

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER,
-  appId: import.meta.env.VITE_APP_APP_ID
+  apiKey: VITE_APP_FIREBASE_API_KEY,
+  authDomain: VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_APP_MESSAGING_SENDER,
+  appId: VITE_APP_APP_ID
 };
 
 // Initialize Firebase
